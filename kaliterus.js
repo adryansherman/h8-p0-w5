31beb389588c5e3642147e4ef6eedd4856fff356
adryansherman <adryansherman@gmail.com>
2019-08-22 12:06:54 +0700

    kaliterus

--- kaliterus.js
+++ kaliterus.js
@@ -3,21 +3,20 @@ function kaliTerusRekursif(angka) {
     if(strAngka.length === 1){
         return angka
     }else{
-        var kali = 1;
+        var next = 1;
         for (i=0; i<strAngka.length; i++){
             if(strAngka.length > 1){
-               kali = kali * Number(strAngka[i])
-               console.log(kali)
+               next = next * Number(strAngka[i])
             }
         }
-        return kali //kaliTerusRekursif(kali)
+        return kaliTerusRekursif(next)
     }
   }
   
   // TEST CASES
   console.log(kaliTerusRekursif(66)); // 8
-  //console.log(kaliTerusRekursif(3)); // 3
-  //console.log(kaliTerusRekursif(24)); // 8 
-  //console.log(kaliTerusRekursif(654)); // 0
-  //console.log(kaliTerusRekursif(1231)); // 6
+  console.log(kaliTerusRekursif(3)); // 3
+  console.log(kaliTerusRekursif(24)); // 8 
+  console.log(kaliTerusRekursif(654)); // 0
+  console.log(kaliTerusRekursif(1231)); // 6
 
